e87b66f7e327906fda5e30ad03a92aba30d4509f
jaturonsangsiri <jaturon_sangsiri@hotmail.com>
2025-08-02 20:46:04 +0700

    add script

---
+++ scripts/calories_buddy_db_backup.sql
@@ -0,0 +1,10 @@
+
+insert into "Foods" (id, name, description, calories, carb, fat, protein, image, "updatedBy", "foodCategory") values
+('cmdpzergq0000vbvoltv8dxyb',	'ไข่ไก่ 1 ฟอง',	'1. ไข่ มีสารอาหารทุกอย่างที่ร่างกายของมนุษย์ต้องการ · 2. ไข่มีคลอเรสเตอรอลสูง แต่เป็นคลอเรสเตอรอลที่ดีต่อสุขภาพ · 3. ช่วยเพิ่มค่าคลอเรสเตอรอล HDL',	72,	0.4,	4.8,		6.3, '',	'adminTest',	'PROTEINS'),
+('cmdpzhbgv0001vbvo0bxb01go',	'ไข่ไก่ 1 ฟอง2',	'1. ไข่ มีสารอาหารทุกอย่างที่ร่างกายของมนุษย์ต้องการ · 2. ไข่มีคลอเรสเตอรอลสูง แต่เป็นคลอเรสเตอรอลที่ดีต่อสุขภาพ · 3. ช่วยเพิ่มค่าคลอเรสเตอรอล HDL',	72,	0.4,	4.8,	6.3, '',	'adminTest',	'PROTEINS'),
+('cmdpzheui0002vbvow3ad02js',	'ไข่ไก่ 1 ฟอง3',	'1. ไข่ มีสารอาหารทุกอย่างที่ร่างกายของมนุษย์ต้องการ · 2. ไข่มีคลอเรสเตอรอลสูง แต่เป็นคลอเรสเตอรอลที่ดีต่อสุขภาพ · 3. ช่วยเพิ่มค่าคลอเรสเตอรอล HDL',	72,	0.4,	4.8,		6.3, '',	'adminTest',	'PROTEINS');
+
+
+insert into "Users" (id, name, "accName", password, email, gender, age, height, weight, "profileImg", "activityLevel", goal, "isActive", carbohydrate, "maxCabohydrate", protien, "maxProtien", fat, "maxFat", calories, tdee, role) values
+('cmdlq32yb0000vbh4qhwoprqv',	'testName',	'test1',	'1234',	'test@hot.com',	'MALE',	25,	167,	60, '',		'MEDIUM',	'GAIN_WEIGHT',	true, 0, 0, 0, 0, 0, 0, 0, 0,	'USER'),
+('cmdlrzpy10008vb5sge92bwt4',	'abc',	'admin1',	'1234',	'tea@hotmail.com',	'MALE',	0,	0,	0, '',		'MEDIUM',	'GAIN_WEIGHT',	true, 0, 0, 0, 0, 0, 0, 0, 0,	'ADMIN');
